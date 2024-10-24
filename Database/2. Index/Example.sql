2b4828d10cef008774cafe4949bde421df4fab40
hongquan <danghongquanvtc1111@gmail.com>
2024-10-24 17:07:51 +0700

    up SQL

--- Database/2. Index/Example.sql
+++ Database/2. Index/Example.sql
@@ -1,9 +1,9 @@
-#Sử dụng Expain để kiểm tra kế hoạch thực thi,
+# Sử dụng Expain để kiểm tra kế hoạch thực thi,
 # nó cho biết chỉ mục có được dùng hay ko
 EXPLAIN SELECT * FROM employees WHERE id = 123;
 
-#Trong MySQL có thể sử dụng "Actual Execution Plan" hoặc "Estimated Execution Plan"
-#Kiểm tra các chỉ mục hiện có và kết hợp với 
-#lệnh EXPLAIN để xem chỉ mục nào đang được sử dụng trong query.
+# Trong MySQL có thể sử dụng "Actual Execution Plan" hoặc "Estimated Execution Plan"
+# Kiểm tra các chỉ mục hiện có và kết hợp với 
+# lệnh EXPLAIN để xem chỉ mục nào đang được sử dụng trong query.
 SHOW INDEXES FROM table_name;
 
